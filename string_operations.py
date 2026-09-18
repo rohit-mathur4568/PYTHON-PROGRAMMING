485e557506c0814595fc2737936b4c698af64bb3
rohit mathur <mathurrohit1674@gmail.com>
2026-09-18 11:46:38 +0530

    String.py

---
+++ string_operations.py
@@ -0,0 +1,17 @@
+name = "Rohit Mathur"
+grade = 'B'
+
+print("name: ", name)
+print(name.upper())
+
+
+#find
+print(name.find("m"))
+print(name.find("M"))
+
+#replace
+
+print(name.replace("Mathur","rathore"))
+
+# check for presence
+print("M" in name)
